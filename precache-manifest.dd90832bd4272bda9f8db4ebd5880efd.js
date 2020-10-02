f7d88f7adcfa65f8d078542fbbf447cdfc52006d
Dmitry Mungalov <d.mungalov@gmail.com>
2020-10-02 10:32:22 +0700

    Updates

---
+++ precache-manifest.dd90832bd4272bda9f8db4ebd5880efd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87811d4de5268494a3eb78f57b5f8b86",
+    "url": "/routes-editor/index.html"
+  },
+  {
+    "revision": "18c187d3354cf71a443c",
+    "url": "/routes-editor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "adde08877cd93df6fcb1",
+    "url": "/routes-editor/static/js/2.ba793139.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/routes-editor/static/js/2.ba793139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c187d3354cf71a443c",
+    "url": "/routes-editor/static/js/main.b5777811.chunk.js"
+  },
+  {
+    "revision": "29ae775fd9d67295fe5a",
+    "url": "/routes-editor/static/js/runtime-main.4778d9a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/routes-editor/static/media/logo.5d5d9eef.svg"
+  }
+]);
