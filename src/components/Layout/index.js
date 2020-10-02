695f1c6eb3d9bf79be8a73ec41be756f02c52d46
Dmitry Mungalov <d.mungalov@gmail.com>
2020-10-02 11:19:01 +0700

    add <Layout>

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <div className="layout__panel">List</div>
+      <div className="layout__panel">Map</div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/Layout/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Layout from "./";
+
+test("renders Layout", () => {
+  const layout = render(<Layout />);
+  expect(layout.container.childElementCount).toBe(1);
+  expect(layout.container.firstChild.childElementCount).toBe(2);
+  expect(layout.container.firstChild.className).toBe("layout");
+  layout.container.firstChild.childNodes.forEach((node) =>
+    expect(node.className).toBe("layout__panel")
+  );
+});
