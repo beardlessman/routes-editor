a5a5be985f0bd758d0d95103ba67afa8dc40fd9a
Dmitry Mungalov <d.mungalov@gmail.com>
2020-10-04 10:17:49 +0700

    add Enzyme and snapshot test for <Layout>

--- src/components/Layout/index.test.tsx
+++ src/components/Layout/index.test.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Layout from "./index";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 
-test("renders Layout", () => {
-  const layout = render(<Layout />);
-  expect(layout.container.childElementCount).toBe(1);
-  expect(layout.container.firstChild.childElementCount).toBe(2);
-  expect(layout.container.firstChild.className).toBe("layout");
-  layout.container.firstChild.childNodes.forEach((node) =>
-    expect(node.className).toBe("layout__panel")
-  );
+describe("<Layout> component", () => {
+  it("Should render component Layout", () => {
+    const component = shallow(<Layout />);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it("Should render 1 '.layout' element inside", () => {
+    const layout = render(<Layout />);
+    expect(layout.container.childElementCount).toBe(1);
+    expect(layout.container.firstChild.className).toBe("layout");
+  });
+
+  it("Should render 2 '.layout__panel' elements inside", () => {
+    const layout = render(<Layout />);
+    expect(layout.container.firstChild.childElementCount).toBe(2);
+
+    layout.container.firstChild.childNodes.forEach((node) =>
+      expect(node.className).toBe("layout__panel")
+    );
+  });
 });
